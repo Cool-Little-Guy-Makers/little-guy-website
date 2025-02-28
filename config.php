9a8b68730f5e93eda071a7b21a012b01fc8fa2ac
lucaguerrera <lguerrera@wesleyan.edu>
2025-02-28 17:26:25 -0500

    Adds the creator page + php (issue #36)
    Allows the user to create a little guy with a name and style which is
    uploaded to the database. The user is USERTEMPLATE and will need to be
    updated once the login functionality is implemented. Also will need to
    redirect to the home page once it has been made.

--- config.php
+++ config.php
@@ -1,18 +1,16 @@
 <?php
 
     // CHANGE THESE FIELDS
-    $hostname = "localhost" // replace with MySQL hostname on control panel
-    $username = "root" // replace with MySQL username on control panel
-    $password = "" // replace with password
+    $hostname = "localhost"; // replace with MySQL hostname on control panel
+    $username = "root"; // replace with MySQL username on control panel
+    $password = ""; // replace with password
 
-    $db = mysqli_connect($hostname,$username,$password,"app-db");
+    $db = mysqli_connect($hostname,$username,$password,"app-db");    
 
     if (mysqli_connect_errno()) {
-        
-    echo "<h1>ERROR: Failed to connect to MySQL: "
-    . mysqli_connect_error() . "<h1>";
+        echo "<h1>ERROR: Failed to connect to MySQL: " . mysqli_connect_error() . "<h1>";
 
-    exit("Connection failed: " . mysqli_connect_error())
+        exit("Connection failed: " . mysqli_connect_error());
     }
 
 ?>
