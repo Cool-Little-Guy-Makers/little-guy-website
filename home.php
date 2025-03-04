c7db1dd581f075f8b0f75f91ee45f324846a8a73
mazmystic <mllabowitz@gmail.com>
2025-03-04 01:05:51 -0500

    Add table for your little guys
    Adds table for your guys with the following columns:
    - ID
    - Name
    - Variant as text
    - Variant as image
    - Actions

--- home.php
+++ home.php
@@ -14,7 +14,7 @@
             session_start();
 
             // Current user credentials
-            $username = "USERTEMPLATE"; // change to $_SESSION["username"]; or equivalent
+            $user = "USERTEMPLATE"; // change to $_SESSION["user"]; or equivalent
             $logged_in = 1; // change to $_SESSION["loggedin"]; or equivalent
             
             // Prevent access to home page if not signed in
@@ -22,34 +22,17 @@
                 header("location: " . $sign_in_page);
             }
 
-            echo "You are logged in as user: " . $username;
+            echo "You are logged in as user: " . $user;
 
-            /*if ($_SESSION["boop"] == "me") {
-                $_SESSION["boop"] = "Sssandra";
-            }
+            // Guy variants as strings
+            $variants[0] = "Blue and Pink";
+            $variants[1] = "Purple and Yellow";
+            $variants[2] = "Green";
 
-            if ($_SESSION["boop"] == "it's") {
-                $_SESSION["boop"] = "me";
-            }
-
-            if ($_SESSION["boop"] == "hello") {
-                $_SESSION["boop"] = "it's";
-            }
-
-            if ($_SESSION["boop"] == "there") {
-                $_SESSION["boop"] = "hello";
-            }
-
-            if ($_SESSION["boop"] == "hi") {
-                $_SESSION["boop"] = "there";
-            }
-
-            if (!isset($_SESSION["boop"])) {
-                $_SESSION["boop"] = "hi";
-            }
-
-            echo "<br>";
-            echo $_SESSION["boop"];*/
+            // Guy variants as images
+            $variant_images[0] = "assets/little-guys/little-guy-trans.png";
+            $variant_images[1] = "assets/little-guys/little-guy-enby.png";
+            $variant_images[2] = "assets/little-guys/little-guy-green.png";
 
         ?>
 
@@ -58,6 +41,55 @@
         <a href="logout.php">Log Out</a>
 
         <br>
+
+        <!--SECTION: YOUR LITTLE GUYS TABLE-->
+
+        <h1>Your Little Guys</h1>
+        
+        <a href="creator.html">Create a new Little Guy</a>
+
+        <table>
+            <!--COLUMN HEADERS-->
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Variant</th>
+                <th>Image</th>
+                <th>Actions</th>
+            </tr>
+
+            <?php
+
+                $edit_guy_page = "editor.php"; // Replace with correct edit guy page if needed
+
+                // Get your little guys
+                $sql = "SELECT * FROM `little-guys` WHERE `username` = (?)";
+                $stmt = mysqli_prepare($db, $sql);
+                mysqli_stmt_bind_param($stmt, "s", $user);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+
+                if ($result == false) die("Failed to get result");
+
+                $row = mysqli_fetch_array($result);
+
+                // $row will equal null when no more rows left
+                while ($row != null) {
+
+                    echo "<tr>";
+                    echo "<td>" . $row[0] . "</td>";
+                    echo "<td>" . $row[2] . "</td>";
+                    echo "<td>" . $variants[$row[3]] . "</td>"; // Variant type as string
+                    echo "<td>" . '<img src="' . $variant_images[$row[3]] . '" alt="Picture of Little Guy" width="90" height="100">'. "</td>"; // Variant type as image
+                    echo "<td>" . '<a href="' . $edit_guy_page . '">Edit</a>' . "</td>";
+                    echo "</tr>";
+
+                    $row = mysqli_fetch_array($result);
+                }
+
+            ?>
+        </table>
+
 
     </body>
 </html>
