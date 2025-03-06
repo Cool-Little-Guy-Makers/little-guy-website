4a462a73c3b250d02868d56667e32fcb0ed73be8
kateralphy <katelyman164@gmail.com>
2025-03-06 14:22:04 -0500

    stopped credentials being added if registration error, prioritized username error message over password matching in registration

--- createaccount.php
+++ createaccount.php
@@ -21,31 +21,31 @@ Prevent registration:
     $result = mysqli_stmt_get_result($stmt);
     $num = mysqli_fetch_array($result);
     
-    if (($password != $passwordconf)) {
-        $_SESSION['signuperror'] = 'pass';
+    if ($num > 0) {
+        $_SESSION['signuperror'] = 'user';
         header("location:" . "registration.php");
     }
-    else if ($num > 0) {
-        $_SESSION['signuperror'] = 'user';
+    else if (($password != $passwordconf)) {
+        $_SESSION['signuperror'] = 'pass';
         header("location:" . "registration.php");
     }
     else {
         $_SESSION['signuperror'] = 'none';
+
+        $sql = "INSERT INTO `users` VALUES (?,?); ";
+        $stmt = mysqli_prepare($db,$sql);
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        mysqli_stmt_bind_param($stmt,"ss", $username, $hashed_password);
+        mysqli_stmt_execute($stmt);
+
+        $_SESSION['user'] = $username;
+        $_SESSION["loggedin"] = true;
+
+        echo "Registration success! Now logged in as $username.";
+        echo "
+            <form action='home.php' method='post'>
+            <input type='submit' value='Take me to the little guys.'>
+            </form>";    
     }
-    
-    $sql = "INSERT INTO `users` VALUES (?,?); ";
-    $stmt = mysqli_prepare($db,$sql);
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    mysqli_stmt_bind_param($stmt,"ss", $username, $hashed_password);
-    mysqli_stmt_execute($stmt);
-
-    $_SESSION['user'] = $username;
-    $_SESSION["loggedin"] = true;
-
-    echo "Registration success! Now logged in as $username.";
-    echo "
-        <form action='home.php' method='post'>
-        <input type='submit' value='Take me to the little guys.'>
-        </form>";    
     
 ?>
