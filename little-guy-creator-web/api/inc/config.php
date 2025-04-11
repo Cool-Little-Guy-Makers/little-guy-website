78020087aa9a71ab93043d2b82f67cf87a4ca73d
kateralphy <katelyman164@gmail.com>
2025-04-11 18:07:35 -0400

    fuck i accidentally committed my auth creds

--- little-guy-creator-web/api/inc/config.php
+++ little-guy-creator-web/api/inc/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USERNAME", "kateralphy");
-define("DB_PASSWORD", "Password time1!");
+define("DB_USERNAME", "user");
+define("DB_PASSWORD", "pass");
 define("DB_DATABASE_NAME", "app-db");
 ?>
