05480a2a3006c8893bddad6896111ee6bad412cf
kateralphy <katelyman164@gmail.com>
2025-04-30 05:39:10 -0400

    other tests

--- little-guy-creator-web/api/test-guys/tests/UserTests.php
+++ little-guy-creator-web/api/test-guys/tests/UserTests.php
@@ -17,18 +17,44 @@ class UserTests extends TestCase
 
    
    public function testPost_CreateUser() {
-    $response = $this->client->request('POST', 'little-guy-website/little-guy-creator-web/api/index.php/user/register');
+    $response = $this->client->request('POST', 'little-guy-website/little-guy-creator-web/api/index.php/user/register', [
+        'headers' => [
+            'Authorization' => 'Bearer your_token_here',
+        ],
+        'json' => [
+            'username' => 'UsernameForTesting',
+            'password' => 'SeCrEtPaSsWoRd1234!@#$',
+        ]
+    ]);
     $this->assertEquals(201, $response->getStatusCode());
     }
 
+    
     public function testPost_LoginUser() {
-        $response = $this->client->request('POST', 'little-guy-website/little-guy-creator-web/api/index.php/user/register');
+        $response = $this->client->request('POST', 'little-guy-website/little-guy-creator-web/api/index.php/user/login', [
+            'headers' => [
+                'Authorization' => 'Bearer your_token_here',
+            ],
+            'json' => [
+                'username' => 'UsernameForTesting',
+                'password' => 'SeCrEtPaSsWoRd1234!@#$',
+            ]
+        ]);
         $this->assertEquals(201, $response->getStatusCode());
     }
 
+    
     public function testPost_Failed() {
-        $response = $this->client->request('POST', 'little-guy-website/little-guy-creator-web/api/index.php/user/register');
-        $this->assertEquals(201, $response->getStatusCode());
+        $response = $this->client->request('POST', 'little-guy-website/little-guy-creator-web/api/index.php/user/login', [
+            'headers' => [
+                'Authorization' => 'Bearer your_token_here',
+            ],
+            'json' => [
+                'username' => 'failUser',
+                'password' => 'short',
+            ]
+        ]);
+        $this->assertEquals(401, $response->getStatusCode());
     }
     
 }
