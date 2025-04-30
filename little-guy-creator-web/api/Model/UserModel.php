b5498f99a85574109c505880dde6c9840a12145e
kateralphy <katelyman164@gmail.com>
2025-04-30 05:05:26 -0400

    preliminary testing environment

--- little-guy-creator-web/api/Model/UserModel.php
+++ little-guy-creator-web/api/Model/UserModel.php
@@ -2,7 +2,11 @@
 require_once PROJECT_ROOT_PATH . "/Model/Database.php";
 class UserModel extends Database
 {
-
+    public function getAllUsers()
+    {
+        return $this->select("SELECT * FROM `users`", []);
+    }
+    
     public function registerUser($username, $password)
     {
         return $this->insert("INSERT INTO users VALUES (?, ?)", ["ss", $username, password_hash($password, PASSWORD_DEFAULT)]);
